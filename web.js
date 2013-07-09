a071ce303ba250f924634be30fe7f6c77a637c44
Seila Kheang <skheang@uw.edu>
2013-07-09 05:11:36 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(reques 2t, response) {
-  response.send('Hello World!');
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
