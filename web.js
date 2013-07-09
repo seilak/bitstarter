2ed5ba70adea6f653248169a70ac5f496a8f68ea
Seila Kheang <skheang@uw.edu>
2013-07-09 05:28:05 +0000

    Using index.html string

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFileSync("index.html");
+  var string = buf.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
